ac5aeee7c1a3a06cd7ca77e2a25eb89ae19cfb7c
Shree <Shree@LAPTOP-VOD9JHKE>
2020-11-23 21:58:16 +0530

    added second java project

--- MyFirstJavaProject/src/myPackage/myFirstJavaclass.java
+++ MyFirstJavaProject/src/myPackage/myFirstJavaclass.java
@@ -1,10 +1,14 @@
 package myPackage;
+import secondPackage.MySecondClass;
 
 public class myFirstJavaclass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		System.out.println("hiii");
+		
+		MySecondClass obj=new MySecondClass();
+		obj.printMyName("Madhuri");
 	}
 
 }
